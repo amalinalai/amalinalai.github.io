c0dee95f4714af449c53d665cc1a4b3911c62b92
Amalina <125463377+amalinalai@users.noreply.github.com>
2024-08-21 19:47:11 +0800

    Restructured toybox

--- scripts/frame-link.js
+++ scripts/frame-link.js
@@ -61,7 +61,10 @@ async function updateHistory() {
     }
 	
 	var fullPathArray = mainFrame.contentWindow.location.pathname.split('/'); // split the path into an array
-	var pagePath = fullPathArray[ fullPathArray.length - 2]; // get the last item in the array (that isn't a space, since the URL ends with /, last element is "")
+	if(fullPathArray[fullPathArray.length-1] == "" ) {  // for folders i.e. "/path/index.html" that renders as "/path/", last item in array is "". Remove this item.
+		fullPathArray.splice(fullPathArray.length-1, 1);
+	}
+	var pagePath = fullPathArray[fullPathArray.length-1]; // get the last item in the array 
 	// pagePath = mainFrame.contentWindow.location.pathname;
     history.replaceState({}, title, "?" + pageParam + "=" + pagePath);
 
